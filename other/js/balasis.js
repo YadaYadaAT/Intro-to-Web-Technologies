9217331bb8a61583648bf20e4bddd67b02e878eb
Balasis <giovani1994a@gmail.com>
2023-12-23 14:41:46 +0200

    AboutMantisesMenuAlmostDone

--- other/js/balasis.js
+++ other/js/balasis.js
@@ -14,21 +14,35 @@
         }
     
 
-   
+   //Setting the listener to the ReadMores. Expanding the divs of their parents.
+
     const ReadMores=document.getElementsByClassName("jHomePageReadMore");
+    const HomePageMiniHolder=document.getElementsByClassName("jHomePageMinimizerHolder");
+    const HomePageMini=document.getElementsByClassName("jHomePageMinimizer");
+
+    for(let e=0;e<HomePageMini.length;e++){
+        HomePageMini[e].addEventListener("click",function(){            
+            this.parentNode.parentNode.classList.remove("jHomePageMenuItemAddHeight");
+            this.parentNode.parentNode.querySelector(".jHomePageMenuItemContent").classList.add("jHomePageMenuItemContentFog");
+            this.parentNode.style.display="none"; 
+            this.parentNode.parentNode.querySelector(".jHomePageReadMore").style.display="flex";
+            this.parentNode.parentNode.scrollIntoView({ behavior: 'smooth' });
+        })
+    }
     for(let k=0;k<ReadMores.length;k++){
         ReadMores[k].addEventListener("click",function(){
-
-            const HomePageMini=document.getElementsByClassName("jHomePageMinimizerHolder");
             for (let qq=0;qq<HomePageMini.length;qq++){
-                HomePageMini[qq].click();
-            }
-
-            this.parentNode.parentNode.parentNode.classList.add("jHomePageMenuItemAddHeight");
-   this.parentNode.parentNode.parentNode.querySelector("jHomePageMenuItemContent").style.setProperty('--before-content', 'none');
-   this.parentNode.parentNode.parentNode.querySelector("jHomePageMenuItemContent").style.setProperty('--after-content', 'none');
+                
+                HomePageMini[qq].parentNode.parentNode.classList.remove("jHomePageMenuItemAddHeight");
+                HomePageMini[qq].parentNode.parentNode.querySelector(".jHomePageMenuItemContent").classList.add("jHomePageMenuItemContentFog");
+                HomePageMini[qq].parentNode.style.display="none"; 
+                HomePageMini[qq].parentNode.parentNode.querySelector(".jHomePageReadMore").style.display="flex";
             
-
+            }
+            this.parentNode.parentNode.parentNode.classList.add("jHomePageMenuItemAddHeight");           
+            this.parentNode.parentNode.parentNode.querySelector(".jHomePageMenuItemContent").classList.remove("jHomePageMenuItemContentFog");
+            this.parentNode.parentNode.parentNode.querySelector(".jHomePageMinimizerHolder").style.display="flex";
+            this.style.display="none";    
         })
     }  
 
