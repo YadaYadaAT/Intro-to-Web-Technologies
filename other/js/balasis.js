e56fd55d28e882dd2b23526501b6fea63aacf66b
Balasis <giovani1994a@gmail.com>
2024-01-07 00:32:47 +0200

    miniGameDone..GoHomButtonMissing,waitingForRescaleFix, will use the main nav of home

--- other/js/balasis.js
+++ other/js/balasis.js
@@ -1085,173 +1085,550 @@ function jjNextIt(){
 
 
 
+(function() {
 
 
-/*
-later....old for colors..
-var secret=document.getElementsByClassName("secretdiv");
-var colors=Array("green","red","purple","yellow","lightblue","orange","blue");
-for (p=0;p<secret.length;p++){
-  var getit=colors[Math.floor(Math.random() * colors.length)];
 
-  secret[p].style.backgroundColor=getit;
-  colors.splice(colors.indexOf(getit),1);
+    document.addEventListener("DOMContentLoaded",function(){
+ if (document.getElementById('jjjjMiniGameIdentifier') !== null){    
 
 
 
 
 
+   
+
+
+
+function jjjjGameSetUp(){
 
 
 
 
 
-function checkwin(){ 
+let jjjjcardItem=document.getElementsByClassName("jjjjcardItem");
+let jjjjCardclasses=Array(
+"Empusidae","Empusidae","Empusidae",
+"Mantidae","Mantidae","Mantidae",
+"Phyllocranidae","Phyllocranidae","Phyllocranidae",
+"Beauty","Beauty","Beauty",
+"AquaMantis","AquaMantis","AquaMantis",
+"funnySayings","funnySayings","funnySayings"
+);
 
-    var arow=document.getElementsByClassName('arow')[0].children;
-  var brow=document.getElementsByClassName('brow')[0].children;
-  var crow=document.getElementsByClassName('crow')[0].children;
-  var drow=document.getElementsByClassName('drow')[0].children;
+for (p=0;p<jjjjcardItem.length;p++){
+  let getit=jjjjCardclasses[Math.floor(Math.random() * jjjjCardclasses.length)];
 
-
-
-  for (as=1;as<arow.length;as++){    
-    if (arow[as].innerHTML == '<img src="images/crown.png">'){      
-    var a="1";            
-    }
-  }
-    for (bs=1;bs<brow.length;bs++){
-    if (brow[bs].innerHTML=='<img src="images/crown.png">'){    
-    var b="1";     
-    }
-  }
-      for (cs=1;cs<crow.length;cs++){
-    if (crow[cs].innerHTML=='<img src="images/crown.png">'){
-    var c="1";   
-    }
-  };
-      for (ds=1;ds<drow.length;ds++){
-    if (drow[ds].innerHTML=='<img src="images/crown.png">'){
-    var d="1";     
-    };
-}
+  jjjjcardItem[p].classList.add(getit);
+  jjjjCardclasses.splice(jjjjCardclasses.indexOf(getit),1);
 
 
 
-
-
-
-if((a=="1")&&(b=="1")&&(c=="1")&&(d=="1")){
-   var releaseHide=document.getElementsByClassName('secretdiv');
- for (secret=0;secret<releaseHide.length;secret++){
-  releaseHide[secret].style.backgroundImage="none";
  }
-  document.getElementById('won').style.display="flex";
 
-
-}
-if ((countdownFinal==0) && !((a=="1")&&(b=="1")&&(c=="1")&&(d=="1")) ){
-
-
- var releaseHide=document.getElementsByClassName('secretdiv');
-alert(releaseHide.length);
- for (secret=0;secret<releaseHide.length;secret++){
-  
-  releaseHide[secret].style.backgroundImage="none";
- }
-  document.getElementById('lost').style.display="flex";
-
-}
-
-}
+let jjjCardsFunnySayingsUrl=Array(
+    "bamboo.jpg",
+    "getALife.jpg",
+    "iAmRightHere.jpg",
+    "LetTheStressBegin.jpg",
+    "meAlwaysRight.jpg",
+    "ReallyFunny.jpg",
+);
 
 
 
+let jjjjRandomFunny=jjjCardsFunnySayingsUrl[Math.floor(Math.random() * jjjCardsFunnySayingsUrl.length)];
 
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-function go(){
-var radar="positive";
-
-for (var h=0;h<boxess.length;h++){
-if (boxess[h].children[countdownFinal].style.backgroundColor=="black"){
-  var radar="negative";
-}
-
-
-
-}
-
-
-
-if (radar=="negative"){
-  alert('fill the squares with colors to continue');  
-}else if ((countdownFinal>0) && (radar="positive")){
-for (var ha=0;ha<boxess.length;ha++){
-if (boxess[ha].children[countdownFinal].style.backgroundColor==boxess[ha].children[0].style.backgroundColor){
-  boxess[ha].children[countdownFinal].innerHTML="<img src='images/crown.png'>";
-
-}
-}
-
-
-
-countdownFinal=countdownFinal-1;
-begone=countdownFinal+1;
-document.getElementsByClassName("velos")[0].children[begone].innerHTML="";
-document.getElementsByClassName("velos")[0].children[countdownFinal].innerHTML="<img src='images/bugshand.png'>";
-
-  for (var q=0;q<boxess.length;q++){
-
-  var divs=boxess[q].children;
-   for (var s=0;s<divs.length;s++){
-
-    if(s==countdownFinal){
-
-      divs[s].style.pointerEvents="auto";
-       divs[s].style.boxShadow="0px 0px 15px white";
-    }else{
-      divs[s].style.pointerEvents="none";
-      divs[s].style.boxShadow="inherit";
+setTimeout(function(){
+  let  jjjjQfunnySayings=document.getElementsByClassName('funnySayings');
+    for (p=0;p<jjjjQfunnySayings.length;p++){
+        jjjjQfunnySayings[p].querySelector('.jjjcardItemFront').style.backgroundImage="url(../images/Balasis/miniGame/funnySayings/"+jjjjRandomFunny;
     }
 
-  }
+},10);
+
+
+
+
+
 }
+    
+
+
+jjjjGameSetUp();
+
+
+let jjjjgameEnded=false;
+
+        //ok ..this is a tricky one because animations of rotation get interuppted with animation of hover(keyframe animation)
+        //in each one of these I need to first add the clicked element to an array (jjjcardiflipped) then
+        //disactivate his animation..then add the rotation...then check if matches or it has to be flipped again
+        //and in case it doesnt match add back the animation with a delay though to have time for the flip to take place..again..
+        //so setTimeout = animation time of flip...
+
+
+        function jjjjSDisplayTopScores() {
+                
+            let jjjjSTopScores = JSON.parse(localStorage.getItem('jjjjSTopScores')) || [];
+        
+           
+            let jjjjSTimeTable = document.getElementById('jjjjSTimeTable');
+        
+           
+            jjjjSTimeTable.innerHTML = '<h5>Top 10 Times</h5>';
+        
+           
+            jjjjSTopScores.forEach((jjjjSScore, index) => {
+                let jjjjSScoreElement = document.createElement('p');
+                jjjjSScoreElement.textContent = `${index + 1}. ${jjjjSScore} seconds`;
+                jjjjSTimeTable.appendChild(jjjjSScoreElement);
+            });
+        }
+
+
+        function jjjjclearJjjjSTopScores() {
+            if (localStorage.getItem('jjjjSTopScores')) {
+                localStorage.removeItem('jjjjSTopScores');
+            }
+             jjjjSDisplayTopScores();
+        }
+
+        jjjjSDisplayTopScores();
+
+
+        let jjjScoresMiniGame=document.getElementById('jjjScoresMiniGame');
+        let  jjjShowMiniGamee=document.getElementById('jjjShowMiniGame');
+       jjjScoresMiniGame.addEventListener('click',function(){
+        jjjjShowTheScores();
+    });
+
+    function jjjShowMiniGame(){
+        if ( jjjjgameEnded==true){
+            jjjjResetMiniGameLet();
+        }
+
+        document.getElementById('jjjShowMiniGame').style.display="none";
+        document.getElementById('jjjScoresMiniGame').style.display="flex";        
+        document.getElementsByClassName('jjjjcardHolder')[0].style.display="grid";
+        document.getElementById('jjjjTableFrame').style.display="none"; 
+       document.getElementById('jjjjCardTimer').style.display="flex";
+
+       
+    }
+
+
+        jjjShowMiniGamee.addEventListener('click',function(){
+            jjjShowMiniGame();
+            
+        });
+
+
+       
+
+        function jjjjShowTheScores(){
+            if ( jjjjgameEnded==true){
+                jjjjResetMiniGameLet();
+            }
+            jjjjSDisplayTopScores();
+            document.getElementById('jjjjCardTimer').style.display="none";
+            document.getElementById('jjjScoresMiniGame').style.display="none";
+            document.getElementById('jjjShowMiniGame').style.display="flex";           
+            document.getElementsByClassName('jjjjcardHolder')[0].style.display="none";
+            document.getElementById('jjjjTableFrame').style.display="flex"; 
+        }
+
+
+
+     
+       
+        document.getElementById('jjjResetMiniGameTable').addEventListener('click',function(){
+            jjjjclearJjjjSTopScores();
+        })
+
+
+
+    let jjjjcardItems = document.getElementsByClassName('jjjjcardItem');
+    let jjjCardiFlipped = [];
+    let jjjjloadSafety=false;
+    let jjjjtimerIsOn=false;
+    let jjjjQuiztimer=0;
+   
+   
+            let jjjjQuiztimerMinutes=0;
+
+
+
+
+
+
+
+    for (let i = 0; i < jjjjcardItems.length; i++) {
+        jjjjcardItems[i].addEventListener('click', function (e) {
+            e.preventDefault();
+        if (jjjjloadSafety==false){
+            jjjjloadSafety=true;
+            
+
+
+            if (jjjCardiFlipped.length === 0) {
+
+              
+
+                jjjCardiFlipped.push(jjjjcardItems[i]);
+                if (jjjjtimerIsOn==false){
+                jjjjStartCardGameTimer();
+            }
+
+                for (let j = 0; j < jjjjcardItems.length; j++) {
+                    if (jjjCardiFlipped.includes(jjjjcardItems[j])) {
+                        jjjjcardItems[j].style.animationPlayState = 'paused';
+                        jjjjcardItems[j].style.animationName = 'none';
+                    } else {
+                        jjjjcardItems[j].style.animationPlayState = '';
+                        jjjjcardItems[j].style.animationName = 'jjjhoverAnimation';
+                    }
+                }
+
+                setTimeout(function(){
+                    jjjjcardItems[i].classList.add('jjjcardItemRotated');
+              },50);
+
+                
+
+                
+              } else if (jjjCardiFlipped.length === 1) {
+                jjjCardiFlipped.push(jjjjcardItems[i]);
+
+                for (let j = 0; j < jjjjcardItems.length; j++) {
+                    if (jjjCardiFlipped.includes(jjjjcardItems[j])) {
+                        jjjjcardItems[j].style.animationPlayState = 'paused';
+                        jjjjcardItems[j].style.animationName = 'none';
+                        
+                    } else {
+                        jjjjcardItems[j].style.animationPlayState = '';
+                        jjjjcardItems[j].style.animationName = 'jjjhoverAnimation';
+                    }
+                }
+
+                setTimeout(function(){
+                jjjjcardItems[i].classList.add('jjjcardItemRotated');
+              },50);
+                
+                if (jjjCardiFlipped[0].classList[1] === jjjCardiFlipped[1].classList[1]) {
+                    
+
+
+
+
+                }
+                
+             
+
+
+              } else if ((jjjCardiFlipped.length === 2)  ) {
+
+                if (jjjCardiFlipped.length<3){
+               
+
+                jjjCardiFlipped.push(jjjjcardItems[i]);
+
+
+                for (let j = 0; j < jjjjcardItems.length; j++) {
+                    if (jjjCardiFlipped.includes(jjjjcardItems[j])) {
+                        jjjjcardItems[j].style.animationPlayState = 'paused';
+                        jjjjcardItems[j].style.animationName = 'none';
+                    } else {
+                        jjjjcardItems[j].style.animationPlayState = '';
+                        jjjjcardItems[j].style.animationName = 'jjjhoverAnimation';
+                    }
+                }
+
+                setTimeout(function(){
+                jjjjcardItems[i].classList.add('jjjcardItemRotated');
+
+              },50);
+
+            
+                if (jjjCardiFlipped[0].classList[1] === jjjCardiFlipped[1].classList[1] &&
+                    jjjCardiFlipped[1].classList[1] === jjjCardiFlipped[2].classList[1]) {
+
+                    // Code to execute when cards match (for 3 cards)
+                  
+
+                   
+                    setTimeout(function(){ 
+
+                    jjjCardiFlipped[0].classList.add('jjjcardItemRotatedRemoved');
+                    jjjCardiFlipped[1].classList.add('jjjcardItemRotatedRemoved');                  
+                    jjjCardiFlipped[2].classList.add('jjjcardItemRotatedRemoved');
+                    let jjjjOnlyOnceCheck=true;
+                    for (let j = 0; j < jjjjcardItems.length; j++) {
+                        if (!jjjjcardItems[j].classList.contains('jjjcardItemRotatedRemoved')) {
+                            jjjjOnlyOnceCheck=false;                            
+                        }
+                    }
+                    jjjjgameEnded=jjjjOnlyOnceCheck;
+                  
+
+                    setTimeout(function () {
+                    if( document.getElementsByClassName('jjjjcardHolder')[0].classList.contains('jjjjcardHolderCorrect')){
+                   document.getElementsByClassName('jjjjcardHolder')[0].classList.remove('jjjjcardHolderCorrect');
+                         }
+
+                 if( document.getElementsByClassName('jjjjcardHolder')[0].classList.contains('jjjjcardHolderFalse')){
+                    document.getElementsByClassName('jjjjcardHolder')[0].classList.remove('jjjjcardHolderFalse');
+                 }
+
+                   document.getElementsByClassName('jjjjcardHolder')[0].classList.add('jjjjcardHolderCorrect');
+
+
+                   setTimeout(function(){
+                    if( document.getElementsByClassName('jjjjcardHolder')[0].classList.contains('jjjjcardHolderCorrect')){
+                        document.getElementsByClassName('jjjjcardHolder')[0].classList.remove('jjjjcardHolderCorrect');
+                              }
+     
+                    },2100);
+                }, 500);
+
+
+
+                        setTimeout(function(){
+                            jjjCardiFlipped = [];
+                        },10);
+                  
+
+              
+                         },1500);
+
+
+                } else {
+
+                    setTimeout(function () {
+                        if( document.getElementsByClassName('jjjjcardHolder')[0].classList.contains('jjjjcardHolderCorrect')){
+                            document.getElementsByClassName('jjjjcardHolder')[0].classList.remove('jjjjcardHolderCorrect');
+                                  }
+         
+                          if( document.getElementsByClassName('jjjjcardHolder')[0].classList.contains('jjjjcardHolderFalse')){
+                             document.getElementsByClassName('jjjjcardHolder')[0].classList.remove('jjjjcardHolderFalse');
+                          }
+         
+                            document.getElementsByClassName('jjjjcardHolder')[0].classList.add('jjjjcardHolderFalse');
+         
+         
+                            setTimeout(function(){
+                             if( document.getElementsByClassName('jjjjcardHolder')[0].classList.contains('jjjjcardHolderFalse')){
+                                 document.getElementsByClassName('jjjjcardHolder')[0].classList.remove('jjjjcardHolderFalse');
+                                       }
+              
+                             },2100);
+    
+                            }, 500);
+                  
+
+                  
+                    setTimeout(function () {
+                      
+
+                    
+
+                       
+                       
+                        for (let j = 0; j < jjjjcardItems.length; j++) {
+                            if (jjjCardiFlipped.includes(jjjjcardItems[j])) {
+                                jjjjcardItems[j].style.animationPlayState = 'paused';
+                                jjjjcardItems[j].style.animationName = 'none';
+                            } else {
+                                jjjjcardItems[j].style.animationPlayState = '';
+                                jjjjcardItems[j].style.animationName = 'jjjhoverAnimation';
+                            }
+                        }
+
+                        setTimeout(function(){
+                            for (let j = 0; j < jjjCardiFlipped.length; j++) {
+                                jjjCardiFlipped[j].classList.remove('jjjcardItemRotated');
+                            }
+                            jjjCardiFlipped = [];
+                        },50)
+                        
+
+
+                       
+
+
+
+                    }, 1500);
+
+                 
+
+
+                }
+
+
+
+
+
+
+          
+
+
+
+
+
+            }
+          
+
+
+              
+
+            }
+
+
+
+            setTimeout(function(){ 
+                jjjjloadSafety=false;
+            },20)
+
+            }
+
+            
+            function jjjjSSaveTopScores(jjjjSScore) {              
+                let jjjjSTopScores = JSON.parse(localStorage.getItem('jjjjSTopScores')) || [];   
+                jjjjSTopScores.push(jjjjSScore);
+                jjjjSTopScores.sort((a, b) => b - a);
+                jjjjSTopScores = jjjjSTopScores.slice(0, 10);
+                localStorage.setItem('jjjjSTopScores', JSON.stringify(jjjjSTopScores));
+            }
+
+           
+        
+
+
+
+
+
+
+
+
+            function jjjjStartCardGameTimer(){
+
+                jjjjtimerIsOn=true;
+                jjjjCountDownInterval=setInterval(function(){
+
+                    if (jjjjQuiztimerMinutes == 0){
+                        if (jjjjQuiztimer<10){
+                            jjjjQuiztimerShow="0"+jjjjQuiztimer;
+                        }else{
+                            jjjjQuiztimerShow=jjjjQuiztimer;
+                        }
+                
+                }else{
+                    if (jjjjQuiztimer<10){
+                        jjjjQuiztimerShow="0"+jjjjQuiztimer;
+                    }else{
+                        jjjjQuiztimerShow=jjjjQuiztimer;
+                    }
+
+                    jjjjQuiztimerShow=jjjjQuiztimerMinutes + " : " + jjjjQuiztimer;
+                }
+                
+                    document.getElementById('jjjjCardTimerText').innerText=jjjjQuiztimerShow;
+
+                    
+                   
+                    console.log(jjjjgameEnded);
+                        if (jjjjgameEnded==true){      
+                                                          
+                          jjjjSSaveTopScores(jjjjQuiztimerShow);
+                          setTimeout(function(){
+
+                            setTimeout(function(){
+                            jjjjSDisplayTopScores() ; 
+                        },20);
+                            jjjScoresMiniGame.click();
+
+                          },50)
+                        
+                          clearInterval(jjjjCountDownInterval);    
+                        
+                          
+                       }
+
+                       jjjjQuiztimer=jjjjQuiztimer+1;
+
+                       if (jjjjQuiztimer==60){
+                         jjjjQuiztimerMinutes=jjjjQuiztimerMinutes+1;
+                         jjjjQuiztimer=0;
+
+                       }
+
+                    }   ,1000);
+            }
+            
+
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+    function jjjjResetMiniGameLet() {
+       
+        if (typeof jjjjCountDownInterval !== 'undefined') {
+            clearInterval(jjjjCountDownInterval);
+        }
+    
+       
+        jjjCardiFlipped = [];
+    
+   
+        let id = window.setTimeout(function() {}, 0);
+        while (id--) {
+            window.clearTimeout(id);
+        }
+    
+       
+        for (let i = 0; i < jjjjcardItems.length; i++) {
+          
+            while (jjjjcardItems[i].classList.length > 1) {
+                jjjjcardItems[i].classList.remove(jjjjcardItems[i].classList.item(1));
+            }
+    
+           
+            jjjjcardItems[i].removeAttribute('style');
+        }
+        jjjjQuiztimer=0;
+        jjjjQuiztimerMinutes=0;
+        jjjjQuiztimerShow=0;
+        document.getElementById('jjjjCardTimerText').innerText=jjjjQuiztimerShow;
+        jjjjGameSetUp();
+    }
+
+
+
+
+
+
+    document.getElementById('jjjResetMiniGame').addEventListener('click',function(){
+        jjjjResetMiniGameLet();
+    })
+
+
+
+
+
+
+
+
 }
 
 
-return countdownFinal;
-}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
+})})();
