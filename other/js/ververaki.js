3b7cb64bffd4f61d0c9beeafaf1a686e1ada6e92
Vertimnus <iro.ikarevrev@gmail.com>
2024-01-07 17:09:06 +0200

    lll

--- other/js/ververaki.js
+++ other/js/ververaki.js
@@ -1,3 +1,13 @@
+// $(window).load(function() {
+//     console.log("works");
+//     $(".btn-nav").on("click tap", function() {
+//       $(".nav-content").toggleClass("showNav hideNav").removeClass("hidden");
+//       $(this).toggleClass("animated");
+//     });
+//   });
+
+
 function changeIcon(anchor) {
-    anchor.classList.toggle("fas fa-toggle-on");
+    
+    anchor.classList.toggle("fa-toggle-on");
 }
