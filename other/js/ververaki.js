b67b402829e93889d6b19a1f3ee12ac3be50c6c1
Vertimnus <iro.ikarevrev@gmail.com>
2024-01-08 18:47:50 +0200

    fixed most of the issues + responsivity

--- other/js/ververaki.js
+++ other/js/ververaki.js
@@ -45,7 +45,6 @@ window.onload = () => {
 
 
     const menu = document.querySelector(".menu");
-    // const menuItems = document.querySelectorAll(".menuItem");
     const hamburger= document.querySelector(".hamburger");
     const closeIcon= document.querySelector(".closeIcon");
     const menuIcon = document.querySelector(".menuIcon");
